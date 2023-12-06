666d8c7064cb463aa1fc6d74ba52970d2899dd70
Exisart <greshnik423>
2023-12-06 18:09:14 +0200

    get personal statistic

--- controller/functions.php
+++ controller/functions.php
@@ -11,3 +11,74 @@ function de($str) {
   echo "</pre>";
   exit;
 }
+
+// get array objects ld from user id
+function getLd($ldList) {
+  $ldArrayObjects = [];
+  $ldArrayId = explode(" ", trim($ldList));
+
+  foreach ($ldArrayId as $ldId) {
+    $ld = new Ld($ldId);
+    $ldArrayObjects[] = $ld;
+  }
+
+  return $ldArrayObjects;
+}
+
+// get summ duration
+function getQuantity($ldArrayObjects) {
+  $summDuration = 0;
+  foreach ($ldArrayObjects as $ld) {
+    $summDuration += intval($ld->duration);
+  }
+
+  return $summDuration;
+}
+
+// get average duration
+function getAverageDuration($ldArrayObjects) {
+  $summDuration = 0;
+  $quantityLd = 0;
+  foreach ($ldArrayObjects as $ld) {
+    if (intval($ld->duration) != 0) {
+      $summDuration += intval($ld->duration);
+      $quantityLd++;
+    }
+  }
+
+  $averageDuration = $summDuration / $quantityLd;
+
+  return round($averageDuration);
+}
+
+// get average quality
+function getAverageQuality($ldArrayObjects) {
+  $summQuality = 0;
+  $quantityLd = 0;
+  foreach ($ldArrayObjects as $ld) {
+    if (intval($ld->duration) != 0) {
+      $summQuality += intval($ld->quality);
+      $quantityLd++;
+    }
+  }
+
+  $averageDuration = $summQuality / $quantityLd;
+
+  return round($averageDuration, 1);
+}
+
+// get average interest
+function getAverageInterest($ldArrayObjects) {
+  $summInterest = 0;
+  $quantityLd = 0;
+  foreach ($ldArrayObjects as $ld) {
+    if (intval($ld->duration) != 0) {
+      $summInterest += intval($ld->interest);
+      $quantityLd++;
+    }
+  }
+
+  $averageInterest = $summInterest / $quantityLd;
+
+  return round($averageInterest, 1);
+}
--- view/parts/main.php
+++ view/parts/main.php
@@ -1,3 +1,24 @@
 <section class="block">
-  main
+  <?php
+  $ldArrayObjects =  getLd($user->ldlist);
+
+  $quantityLd = count($ldArrayObjects);
+  $averageDuration = getAverageDuration($ldArrayObjects);
+  $averageQuality = getAverageQuality($ldArrayObjects);
+  $averageInterest = getAverageInterest($ldArrayObjects);
+
+  $summDuration = getQuantity($ldArrayObjects);
+  $hoursDuration = floor($summDuration / 3600);
+  $minutesDuration = floor(($summDuration % 3600) / 60);
+  $secondsDuration = $summDuration % 60;
+
+
+  ?>
+
+  <p>Всего ОСов: <?=$quantityLd?></p>
+  <p>Общая длительность: <?php echo "$hoursDuration ч, $minutesDuration мин, $secondsDuration сек";?></p>
+  <p>Средняя длительность: <?=$averageDuration?> сек</p>
+  <p>Среднее качество: <?=$averageQuality?></p>
+  <p>Средняя интересность: <?=$averageInterest?></p>
+  <p>Последний был:</p>
 </section>
