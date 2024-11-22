469d86b10cdbd453358b2403078af858629cbee9
Exisart <greshnik423>
2024-11-22 11:15:24 +0200

    css fix

--- view/pages/note/faq.php
+++ view/pages/note/faq.php
@@ -6,111 +6,114 @@
 
 <h2 class="pb-5"><?= htmlspecialchars($title) ?></h2>
 
-<div class="container mt-5">
-  <div class="accordion" id="exampleAccordion">
-    <div class="accordion-item">
-      <h2 class="accordion-header" id="heading1">
-        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse1" aria-expanded="true" aria-controls="collapse1">
-          <p>Что такое проект Lucid?</p>
-        </button>
-      </h2>
-      <div id="collapse1" class="accordion-collapse collapse show" aria-labelledby="heading1" data-bs-parent="#exampleAccordion">
-        <div class="accordion-body">
-          <p>Lucid — это сообщество практиков и энтузиастов осознанных сновидений, а также удобный инструмент для записи и анализа личного опыта.</p>
+<div class="row">
+  <div class="col-md-10 mt-5">
+    <div class="accordion" id="exampleAccordion">
+      <div class="accordion-item">
+        <h2 class="accordion-header" id="heading1">
+          <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse1" aria-expanded="true" aria-controls="collapse1">
+            <p>Что такое проект Lucid?</p>
+          </button>
+        </h2>
+        <div id="collapse1" class="accordion-collapse collapse show" aria-labelledby="heading1" data-bs-parent="#exampleAccordion">
+          <div class="accordion-body">
+            <p>Lucid — это сообщество практиков и энтузиастов осознанных сновидений, а также удобный инструмент для записи и анализа личного опыта.</p>
+          </div>
         </div>
       </div>
-    </div>
 
-    <div class="accordion-item">
-      <h2 class="accordion-header" id="heading2">
-        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse2" aria-expanded="false" aria-controls="collapse2">
-          <p>Какая цель проекта Lucid?</p>
-        </button>
-      </h2>
-      <div id="collapse2" class="accordion-collapse collapse" aria-labelledby="heading2" data-bs-parent="#exampleAccordion">
-        <div class="accordion-body">
-          <p>Цель проекта — популяризация осознанных сновидений и создание платформы для поиска единомышленников, участия в групповых практиках, организации встреч, практикумов и экспериментов.</p>
+      <div class="accordion-item">
+        <h2 class="accordion-header" id="heading2">
+          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse2" aria-expanded="false" aria-controls="collapse2">
+            <p>Какая цель проекта Lucid?</p>
+          </button>
+        </h2>
+        <div id="collapse2" class="accordion-collapse collapse" aria-labelledby="heading2" data-bs-parent="#exampleAccordion">
+          <div class="accordion-body">
+            <p>Цель проекта — популяризация осознанных сновидений и создание платформы для поиска единомышленников, участия в групповых практиках, организации встреч, практикумов и экспериментов.</p>
+          </div>
         </div>
       </div>
-    </div>
 
-    <div class="accordion-item">
-      <h2 class="accordion-header" id="heading3">
-        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse3" aria-expanded="false" aria-controls="collapse3">
-          <p>Какая польза мне от проекта Lucid?</p>
-        </button>
-      </h2>
-      <div id="collapse3" class="accordion-collapse collapse" aria-labelledby="heading3" data-bs-parent="#exampleAccordion">
-        <div class="accordion-body">
-          <p>Выход на новый уровень продуктивности в мире сновидений. Улучшение навыков осознанных сновидений за счет групповой практики, которая эффективнее индивидуальной. Общение с единомышленниками увеличивает вовлеченность и результативность, а новые тематические знакомства делают жизнь ярче и интереснее.</p>
+      <div class="accordion-item">
+        <h2 class="accordion-header" id="heading3">
+          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse3" aria-expanded="false" aria-controls="collapse3">
+            <p>Какая польза мне от проекта Lucid?</p>
+          </button>
+        </h2>
+        <div id="collapse3" class="accordion-collapse collapse" aria-labelledby="heading3" data-bs-parent="#exampleAccordion">
+          <div class="accordion-body">
+            <p>Выход на новый уровень продуктивности в мире сновидений. Улучшение навыков осознанных сновидений за счет групповой практики, которая эффективнее индивидуальной. Общение с единомышленниками увеличивает вовлеченность и результативность, а новые тематические знакомства делают жизнь ярче и интереснее.</p>
+          </div>
         </div>
       </div>
-    </div>
 
-    <div class="accordion-item">
-      <h2 class="accordion-header" id="heading4">
-        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse4" aria-expanded="false" aria-controls="collapse4">
-          <p>В чем разница LucidNote и LucidNet?</p>
-        </button>
-      </h2>
-      <div id="collapse4" class="accordion-collapse collapse" aria-labelledby="heading4" data-bs-parent="#exampleAccordion">
-        <div class="accordion-body">
-          <p>LucidNote - это онлайн дневник осознанных сновидений. LucidNet - социальная сеть с базовым функционалом.</p>
+      <div class="accordion-item">
+        <h2 class="accordion-header" id="heading4">
+          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse4" aria-expanded="false" aria-controls="collapse4">
+            <p>В чем разница LucidNote и LucidNet?</p>
+          </button>
+        </h2>
+        <div id="collapse4" class="accordion-collapse collapse" aria-labelledby="heading4" data-bs-parent="#exampleAccordion">
+          <div class="accordion-body">
+            <p>LucidNote - это онлайн дневник осознанных сновидений. LucidNet - социальная сеть с базовым функционалом.</p>
+          </div>
         </div>
       </div>
-    </div>
 
-    <div class="accordion-item">
-      <h2 class="accordion-header" id="heading5">
-        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse5" aria-expanded="false" aria-controls="collapse5">
-          <p>Какой функционал есть в проекте LucidNote?</p>
-        </button>
-      </h2>
-      <div id="collapse5" class="accordion-collapse collapse" aria-labelledby="heading5" data-bs-parent="#exampleAccordion">
-        <div class="accordion-body">
-          <p>LucidNote позволяет вести дневник осознанных сновидений, анализировать статистику и сопоставлять свои результаты с общими показателями проекта.</p>
+      <div class="accordion-item">
+        <h2 class="accordion-header" id="heading5">
+          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse5" aria-expanded="false" aria-controls="collapse5">
+            <p>Какой функционал есть в проекте LucidNote?</p>
+          </button>
+        </h2>
+        <div id="collapse5" class="accordion-collapse collapse" aria-labelledby="heading5" data-bs-parent="#exampleAccordion">
+          <div class="accordion-body">
+            <p>LucidNote позволяет вести дневник осознанных сновидений, анализировать статистику и сопоставлять свои результаты с общими показателями проекта.</p>
+          </div>
         </div>
       </div>
-    </div>
 
-    <div class="accordion-item">
-      <h2 class="accordion-header" id="heading6">
-        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse6" aria-expanded="false" aria-controls="collapse6">
-          <p>Какой функционал есть в проекте LucidNet?</p>
-        </button>
-      </h2>
-      <div id="collapse6" class="accordion-collapse collapse" aria-labelledby="heading6" data-bs-parent="#exampleAccordion">
-        <div class="accordion-body">
-          <p>LucidNet — это социальная сеть, которая помогает находить единомышленников и группы по интересам или регионам, а также создавать собственные группы для общения и совместных практик.</p>
+      <div class="accordion-item">
+        <h2 class="accordion-header" id="heading6">
+          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse6" aria-expanded="false" aria-controls="collapse6">
+            <p>Какой функционал есть в проекте LucidNet?</p>
+          </button>
+        </h2>
+        <div id="collapse6" class="accordion-collapse collapse" aria-labelledby="heading6" data-bs-parent="#exampleAccordion">
+          <div class="accordion-body">
+            <p>LucidNet — это социальная сеть, которая помогает находить единомышленников и группы по интересам или регионам, а также создавать собственные группы для общения и совместных практик.</p>
+          </div>
         </div>
       </div>
-    </div>
 
-    <div class="accordion-item">
-      <h2 class="accordion-header" id="heading7">
-        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse7" aria-expanded="false" aria-controls="collapse7">
-          <p>Есть ли режим инкогнито в проекте?</p>
-        </button>
-      </h2>
-      <div id="collapse7" class="accordion-collapse collapse" aria-labelledby="heading7" data-bs-parent="#exampleAccordion">
-        <div class="accordion-body">
-          <p>Да, вы можете активировать «Приватный аккаунт» в настройках, чтобы оставаться невидимым для других пользователей.</p>
+      <div class="accordion-item">
+        <h2 class="accordion-header" id="heading7">
+          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse7" aria-expanded="false" aria-controls="collapse7">
+            <p>Есть ли режим инкогнито в проекте?</p>
+          </button>
+        </h2>
+        <div id="collapse7" class="accordion-collapse collapse" aria-labelledby="heading7" data-bs-parent="#exampleAccordion">
+          <div class="accordion-body">
+            <p>Да, вы можете активировать «Приватный аккаунт» в настройках, чтобы оставаться невидимым для других пользователей.</p>
+          </div>
         </div>
       </div>
-    </div>
 
-    <div class="accordion-item">
-      <h2 class="accordion-header" id="heading8">
-        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse8" aria-expanded="false" aria-controls="collapse8">
-          <p>Есть ли в проекте Lucid чат или другая возможность общения?</p>
-        </button>
-      </h2>
-      <div id="collapse8" class="accordion-collapse collapse" aria-labelledby="heading8" data-bs-parent="#exampleAccordion">
-        <div class="accordion-body">
-          <p>Внутреннего чата или мессенджера нет. Проект сосредоточен на поиске людей и создании групп. Все общение происходит в мессенджерах, которые участники указывают в своих профилях или в описаниях групп.</p>
+      <div class="accordion-item">
+        <h2 class="accordion-header" id="heading8">
+          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse8" aria-expanded="false" aria-controls="collapse8">
+            <p>Есть ли в проекте Lucid чат или другая возможность общения?</p>
+          </button>
+        </h2>
+        <div id="collapse8" class="accordion-collapse collapse" aria-labelledby="heading8" data-bs-parent="#exampleAccordion">
+          <div class="accordion-body">
+            <p>Внутреннего чата или мессенджера нет. Проект сосредоточен на поиске людей и создании групп. Все общение происходит в мессенджерах, которые участники указывают в своих профилях или в описаниях групп.</p>
+          </div>
         </div>
       </div>
-    </div>
 
+    </div>
   </div>
 </div>
+
