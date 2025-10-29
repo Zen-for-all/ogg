d12f40353f77a90a2a5c4a0d1d45b1bb09a3f8e5
Exisart <greshnik423>
2025-10-29 16:44:55 +0200

    fix empty ld public list

--- view/pages/net/profile.php
+++ view/pages/net/profile.php
@@ -8,12 +8,17 @@ $profile_owner = 0;
 <a class="btn btn-light mb-4" href="/people"><- К списку участников</a>
 
 <?php
-include 'view/parts/net/profile_data.php';
+  include 'view/parts/net/profile_data.php';
 
-if ($user->ldlist !== null) {
-  $ldArray = json_decode($user->ldlist, true);
+  if ($user->ldlist !== null) {
+    // Get IDs all lds
+    $ldArray = json_decode($user->ldlist, true);
 
-  $ldArray = getPublicLdFromArray($ldArray);
-  include 'view/parts/net/post_list.php';
-}
+    // Get IDs public lds
+    $ldArray = getPublicLdFromArray($ldArray);
+
+    if (is_array($ldArray) && count($ldArray) > 0) {
+      include 'view/parts/net/post_list.php';
+    }
+  }
 ?>
